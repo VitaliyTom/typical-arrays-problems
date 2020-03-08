8028a2d0653ea51b61c0b174731bf2748ffe0860
VitaliyTom <tomashevich.vitaliy88@gmail.com>
2020-03-08 20:58:08 +0300

    feat: add solution typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array) {
+	if (typeof array == 'undefined' || !array.length) {
+		return 0;
+	}
+	let min = 9999;
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] < min) {
+			min = array[i];
+		}
+	}
+	return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+	if (typeof array == 'undefined' || !array.length) {
+		return 0;
+	}
+	let max = -9999;
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] > max) {
+			max = array[i];
+		}
+	}
+	return max;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+	if (typeof array == 'undefined' || !array.length) {
+		return 0;
+	}
+	let avg = 0;
+	for (let i = 0; i < array.length; i++) {
+		avg = avg + array[i];
+	}
+	return avg / array.length;
+};
